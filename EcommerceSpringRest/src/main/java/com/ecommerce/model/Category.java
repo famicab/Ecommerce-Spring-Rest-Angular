138e3f7767f791a4f4b6bfab69f304dd70e82807
famicab <cesco.deamicis@gmail.com>
2024-03-02 17:14:55 +0100

    Add Hibernate basic configuration

--- EcommerceSpringRest/src/main/java/com/ecommerce/model/Category.java
+++ EcommerceSpringRest/src/main/java/com/ecommerce/model/Category.java
@@ -1,14 +1,30 @@
 package com.ecommerce.model;
 
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "CATEGORY")
 public class Category {
 
+	@Id
+	@Column(name="ID_CATEGORY")
 	private long id;
+	@Column
 	private String name;
+	@Column
 	private String description;
+	
+	@OneToMany(mappedBy = "category")
+	private Set<Product> products;
 	
 }
--- EcommerceSpringRest/src/main/java/com/ecommerce/model/Product.java
+++ EcommerceSpringRest/src/main/java/com/ecommerce/model/Product.java
@@ -1,14 +1,35 @@
 package com.ecommerce.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@Column(name="ID_PRODUCT")
 	private long id;
+	
+	@Column
 	private String name;
+	
+	@Column
 	private String description;
+	
+	@JoinColumn(name = "ID_CATEGORY")
+	@ManyToOne(fetch=FetchType.LAZY)
 	private Category category;
 }
