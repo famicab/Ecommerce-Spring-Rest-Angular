ea2d2d31edda85598094bef365c286f875222727
famicab <cesco.deamicis@gmail.com>
2024-04-13 12:05:25 +0200

    Add SecurityFilterChain

---
+++ EcommerceSpringRest/src/main/java/com/ecommerce/security/CustomBasicAuthenticationEntryPoint.java
@@ -0,0 +1,42 @@
+package com.ecommerce.security;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.error.ApiError;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CustomBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint{
+
+	private final ObjectMapper mapper;
+	
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException {
+
+		response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+		response.setContentType("application/json");
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		
+		ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, authException.getMessage());
+		String strApiError = mapper.writeValueAsString(apiError);
+		
+		PrintWriter writer = response.getWriter();
+		writer.println(strApiError);
+	}
+	
+	public void initRealmname() {
+		setRealmName("myecommerce.com");
+	}
+}
--- EcommerceSpringRest/src/main/java/com/ecommerce/security/SecurityConfig.java
+++ EcommerceSpringRest/src/main/java/com/ecommerce/security/SecurityConfig.java
@@ -2,20 +2,44 @@ package com.ecommerce.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-
+	
+	private final CustomBasicAuthenticationEntryPoint customBasicAuthenticationEntryPoint;
+ 
 	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer(){
-		return (web) -> web.ignoring().anyRequest();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+				
+				authorizationManagerRequestMatcherRegistry
+					.requestMatchers("/category/**").permitAll()
+					.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+					.requestMatchers("/admin/**").hasAnyRole("ADMIN")
+					.requestMatchers("/login/**").permitAll()
+					.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.sessionManagement(httpSecuritySessionManagementConfigurer -> 
+				httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				
+		return http.build();
 	}
 }
