ec2de9b0d2a7a8cf9f659828c51995b0143c339d
famicab <cesco.deamicis@gmail.com>
2024-04-14 12:28:49 +0200

    Add FileController and PaginationUtils

---
+++ EcommerceSpringRest/src/main/java/com/ecommerce/upload/FileSystemStorageService.java
@@ -0,0 +1,90 @@
+package com.ecommerce.upload;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileSystemStorageService implements StorageService{
+
+	private final Path rootLocation;
+	
+	public FileSystemStorageService(@Value("${upload.root-location}") String path) {
+		this.rootLocation = Paths.get(path);
+	}
+	
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+    public String store(MultipartFile file) {
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        String extension = StringUtils.getFilenameExtension(filename);
+        String justFilename = filename.replace("."+extension, "");
+        String storedFilename = System.currentTimeMillis() + "_" + justFilename + "." + extension;
+        try {
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file " + filename);
+            }
+            if (filename.contains("..")) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file with relative path outside current directory "
+                                + filename);
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, this.rootLocation.resolve(storedFilename),
+                    StandardCopyOption.REPLACE_EXISTING);
+                return storedFilename;
+            }
+        }
+        catch (IOException e) {
+            throw new StorageException("Failed to store file " + filename, e);
+        }
+        
+    }
+
+	@Override
+	public Stream<Path> loadAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Path load(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource loadAsResource(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(String filename) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
